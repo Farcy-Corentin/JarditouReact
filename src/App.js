0c258d132d9740f85ddc6b65a97fd28de79e2838
Corentin Farcy <corentinfarcy1@gmail.com>
2021-06-12 00:55:34 +0200

    Update Product, Products, App Add product with lib commerce.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { commerce } from './lib/commerce'
 import { Products, NavBar } from './components'
+import { useState, useEffect } from "react";
 
 function App() {
+    const [products, setProducts] = useState([])
+    const fetchProduct = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+
+    console.log(products);
+
   return (
     <div className="App">
         <NavBar />
-       <Products />
+       <Products products={products} />
     </div>
   );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
-const products = [
-    {id: 1, name: "Barbecu1", description: 'un test', price: '50€', image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vX2kyO38GGrwyT-McF79gwHaHa%26pid%3DApi&f=1'},
-    {id:2, name: "Barbecu2", description: 'deux test', price: '100€', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.iUSDFuiMLI9iC5pIOGGW1AHaHa%26pid%3DApi&f=1'}
-]
+const Products =  ({ products } ) => {
+    const classes = useStyles();
 
-const Products =  () => {
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} lg={3}>
@@ -18,7 +17,7 @@ const Products =  () => {
                 ))}
             </Grid>
         </main>
-        )
+    )
 }
 
 export default Products;
